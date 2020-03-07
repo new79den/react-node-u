8f67ee8f9a44b78f5f457656ca05d914bb61729d
denis.korniychuk <new79den@gmail.com>
2020-03-07 12:17:57 +0200

    firs commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+   res.send({
+       test: 'test'
+   })
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
